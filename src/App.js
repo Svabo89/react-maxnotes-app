7e4d594989bd552bf9f0d8bde7c7c626a020ad54
EIMlikota <emanuel.mlikota@scheer-group.com>
2023-01-04 15:09:17 +0100

    added fun comment to fields for Max

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ const App = () => {
   const [notes, setNotes] = useState([
     {
       id: nanoid(),
-      header: "My new note",
+      header: "Works better in a array",
       text: "This is my first note",
       date: "02/01/2023",
     },
     {
       id: nanoid(),
-      header: "My new note",
+      header: "dont you think Max?",
       text: "This is my second note",
       date: "02/01/2023",
     },
     {
       id: nanoid(),
-      header: "My new note",
+      header: "still some work to do",
       text: "This is my third note",
       date: "02/01/2023",
     },
     {
       id: nanoid(),
-      header: "My new note",
+      header: "But im getting there :)",
       text: "This is my fourth note",
       date: "02/01/2023",
     },
