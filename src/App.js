d61232688d235fe16a4fce17e767f2344cf45f89
EIMlikota <emanuel.mlikota@scheer-group.com>
2023-01-04 11:37:43 +0100

    added hook to and id propertis to

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import NotesList from "./components/NotesList";
+import { useState } from "react"; // this provides a hook that allows us to use state in functional components
+import { nanoid } from "nanoid";// this provides a unique id for each note
 const App = () => {
+  const [notes, setNotes] = useState([{ //this function allows us to use state in functional components
+    id: nanoid(), // this provides a unique id for each note
+    titel: "My new note",
+    text: "This is my note",
+    date: "02/01/2023",
+  }]); // this is a hook that allows us to use state in functional components
   return (
     <div className="container">
       <NotesList />
