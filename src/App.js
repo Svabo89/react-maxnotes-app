257f9037dd1ba3cc4cb797372b78033898541005
EIMlikota <emanuel.mlikota@scheer-group.com>
2023-01-10 12:08:47 +0100

    update index.css bas info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-
+import React from'react';
+import ReactDOM from'react-dom';
+import Header from './components/Header';
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div>
+      <Header />
+    </div>
   );
  
 };
