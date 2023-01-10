9fb0f1a253f22141e4dc104a301036990951beca
EIMlikota <emanuel.mlikota@scheer-group.com>
2023-01-10 12:47:14 +0100

    Created JSDoc for reportErrorToBackend.j

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from'react';
 import ReactDOM from'react-dom';
 import Header from './components/Header';
+import Input from './components/input';
 const App = () => {
   return (
     <div>
       <Header />
+      <Input/>
     </div>
   );
  
